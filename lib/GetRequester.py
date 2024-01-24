9446ff8e0f7c990cb30e4de6e78e758d1c7a8868
THITAI <evansnt9619@gmail.com>
2024-01-24 18:15:48 +0300

    lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        my_res = GetRequester.get_response_body(self)
+        jsoned = json.loads(my_res)
+        return jsoned
